aff1e7f9aa577f635d63dd45ddb89994a7baf1a4
raghav <raghavlane@gmail.com>
2020-12-22 01:10:31 -0800

    mail functionality and config file

---
+++ config.py
@@ -0,0 +1,22 @@
+search_string = 'table lamp'
+pin_code = 'V6B1A9'
+search_distance = '40'
+min_price = '0'
+maxprice = '50'
+
+craigslist_url = 'https://vancouver.craigslist.org/search/sss'
+period_check_in_mins = 5
+
+email_mode = True
+smtp_user = '<user@domain.com>'
+smtp_password = '<password>'
+recipient_email = '<user@gmail.com'
+email_subject = 'Craigslist New Postings Found'
+smtp_url = '<smtp server url>'
+smtp_port = 465
+
+twilio_mode = False
+twilio_account_sid = '<account sid from Twilio account>'
+twilio_auth_token  = '<auth token from Twilio account>'
+from_phone_number='<From phone number eg. +19876543210 where +1 is country code and phone is 987-654-3210>'
+to_phone_number='<recipient phone number eg. +19876543210 where +1 is country code and phone is 987-654-3210>'
--- craig.py
+++ craig.py
@@ -1,11 +1,24 @@
-import urllib.request
+import requests
 import time
 import smtplib
+import config
 
 def get_list (url):
-	r = urllib.request.urlopen(url)
-	a = str(r.read())
-	a = (a.split('ul class=\"rows\"')[1]).split('\/div')[0].split(' class=\"result-row\" data-pid=\"')
+	url = "https://vancouver.craigslist.org/search/sss"
+	querystring = {"query":config.search_string,
+		"sort":"date",
+		"hasPic":"1",
+		"postedToday":"1",
+		"search_distance":config.search_distance,
+		"postal":config.pin_code,
+		"min_price":config.min_price,
+		"max_price":config.maxprice
+	}
+	headers = {
+		'cache-control': "no-cache"
+	}
+	response = requests.request("GET", url, headers=headers, params=querystring).text
+	a = (response.split('ul class=\"rows\"')[1]).split('\/div')[0].split(' class=\"result-row\" data-pid=\"')
 	idUrlMap = {}
 	for i in range(1,len(a)):
 		idUrlMap[a[i].split('\"')[0]] = a[i].split('a href=\"')[1].split('\"')[0]
@@ -13,48 +26,44 @@ def get_list (url):
 
 def sendMess(mess):
 	from twilio.rest import Client
-	# Your Account SID from twilio.com/console
-	account_sid = "************************************"
-	# Your Auth Token from twilio.com/console
-	auth_token  = "************************************"
-	client = Client(account_sid, auth_token)
+	client = Client(config.twilio_account_sid, config.twilio_auth_token)
 	message = client.messages.create(
-		to="*****************************************", 
-		from_="**************************************",
+		to=config.to_phone_number, 
+		from_=config.from_phone_number,
 		body=mess)
 	print(message.sid)
 
 def sendEmail(message="test message"):
-	gmail_user = '*********************************'
-	gmail_password = '**********************************'
 
-	sent_from = gmail_user
-	to = ['***************************************']
-	subject = 'craigslist new postings'
+	from email.mime.application import MIMEApplication
+	from email.mime.text import MIMEText
+	from email.mime.multipart import MIMEMultipart
+	from email.header import Header
+	from email.utils import formataddr
 
-	email_text = """\
-	From: %s
-	To: %s
-	Subject: %s
+	smtp_user = config.smtp_user
+	smtp_password = config.smtp_password
+	recipient = config.recipient_email
+	subject = config.email_subject
 
-	%s
-	""" % (sent_from, ", ".join(to), subject, message)
-
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = subject
+	msg['From'] = formataddr((str(Header('Craigslist User', 'utf-8')), 'admin@raghavlane.com'))
+	msg['To'] = recipient
 	try:
-		server = smtplib.SMTP_SSL('cpanel.freehosting.com', 465)
+		server = smtplib.SMTP_SSL(config.smtp_url, config.smtp_port)
 		server.ehlo()
-		server.login(gmail_user, gmail_password)
-		server.sendmail(sent_from, to, email_text)
+		server.login(smtp_user, smtp_password)
+		server.sendmail(smtp_user, recipient, message)
 		server.close()
 	except Exception as e:
 		print('Exception occured ')
 		print(e)
 
-
-url = 'https://vancouver.craigslist.org/search/sss?query=table+lamp&sort=date&hasPic=1&postedToday=1&search_distance=40&postal=v5r4k1&min_price=20&max_price=150'
+url = config.craigslist_url
 processedIdUrlMap = get_list(url)
-while (True): #timeout
-	time.sleep(300)
+while (True):
+	time.sleep(config.period_check_in_mins *60 )
 	print("hitting the URL")
 	currentIdUrlMap = get_list(url)
 	message = ""
@@ -67,4 +76,7 @@ while (True): #timeout
 			message = message + currentIdUrlMap[ids] + ' \n '
 	if (message >""):
 		print(message)
-		sendMess(message)
+		if (config.email_mode):
+			sendEmail(message)
+		if (config.twilio_mode):
+			sendMess(message)
